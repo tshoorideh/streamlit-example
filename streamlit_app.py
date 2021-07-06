6fe70536f8b777e158e3d6730c6265f77587a4aa
tshoorideh <69776499+tshoorideh@users.noreply.github.com>
2021-07-05 23:37:10 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Defining Classes!
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 If you have any questions, checkout out our [documentation](https://docs.streamlit.io) and
 [community forums](https://discuss.streamlit.io).
